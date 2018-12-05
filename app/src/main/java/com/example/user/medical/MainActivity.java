b76b9c6064ca2770f859bbc38e48bab2ba5a2320
eric101784 <eric31806@gmail.com>
2018-12-06 02:59:42 +0800

    [宗勳&金諺]主頁和換頁againQQ

--- app/src/main/java/com/example/user/medical/MainActivity.java
+++ app/src/main/java/com/example/user/medical/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.user.medical;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button GuestBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GuestBtn=(Button)findViewById(R.id.GuestBtn);
+
+
+        Button btn1=(Button)findViewById(R.id.GuestBtn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(MainActivity.this,MainSelectionActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //666666
         //123;
