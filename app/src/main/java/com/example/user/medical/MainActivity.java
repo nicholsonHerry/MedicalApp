8d1d53e4bd348465e32f711748e1c5dbc385a07b
eric101784 <eric31806@gmail.com>
2018-12-06 02:20:11 +0800

    Revert "[宗勳與金諺]Android 主頁面建立與第二頁"
    This reverts commit dc2ff25d1cd75a67706a7949ce0ea15a5c3204ca.

--- app/src/main/java/com/example/user/medical/MainActivity.java
+++ app/src/main/java/com/example/user/medical/MainActivity.java
@@ -1,33 +1,16 @@
 package com.example.user.medical;
 
-import  android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button Guest;  //宣告Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Guest=(Button) findViewById(R.id.Guest); //變數連接Button
 
-
-
-        Button BtnSecondPage=(Button)findViewById(R.id.Guest);
-        BtnSecondPage.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,MainActivity2.class);  //從MainActivity連接到MainActivity2
-                startActivity(intent);
-            }
-        });
+        //123;
     }
 }
