31f8ba3a1f46457e8cd42b855bdd623b56d2f317
blackhank <45022782+blackhank@users.noreply.github.com>
2018-12-07 11:15:13 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/user/medical/MainActivity.java
+++ app/src/main/java/com/example/user/medical/MainActivity.java
@@ -2,6 +2,19 @@ package com.example.user.medical;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,11 +22,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BA = BluetoothAdapter.getDefaultAdapter();
 
 
         //8888
         //11111
         //666666
         //123;
+    }
+    public void on(View view) {      //確認藍牙是否開啟
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on"
+                    , Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
